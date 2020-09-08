9dbc9155a05c2ec900b3d5ecdec1c1bb01355d90
shivramsuthar <shivrfd.suthar@gmail.com>
2020-09-08 09:02:41 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+const http = require('http');
+const port = process.env.PORT || 3000
 
 ReactDOM.render(
   <React.StrictMode>
